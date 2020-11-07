0bc835e33c8e51a8899cd5747c8d7aaae0b59f11
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2020-11-06 18:26:18 -0600

    add tests for helmet config

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,9 +6,40 @@
 *       (if additional are added, keep them at the very end!)
 */
 
+
+var chaiHttp = require('chai-http');
 var chai = require('chai');
 var assert = chai.assert;
+var server = require('../server');
 
-suite('Unit Tests', function(){
+chai.use(chaiHttp);
 
+suite('Unit Tests', function() {
+  suite('Test Helmet configuration', function() {
+    test("Only allow your site to be loading in an iFrame on your own pages", function(done) {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          assert.isTrue(res.headers['x-frame-options'].includes('SAMEORIGIN'));
+          done();
+        })
+    });
+    test("Do not allow DNS prefetching.", function(done) {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          assert.isTrue(res.headers['x-dns-prefetch-control'].includes('off'));
+          done();
+        })
+    });
+    test("Only allow your site to send the referrer for your own pages.", function(done) {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          assert.isTrue(res.headers['referrer-policy'].includes('same-origin'))
+          done();
+        })
+    });
+
+  })
 });
